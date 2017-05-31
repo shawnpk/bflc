979f51b25f128175f8259e92e1e0cef6b2e24876
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-31 15:36:43 -0700

    Change text from blog to article

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def button_text
+    if controller.action_name == "new"
+       return "Create Article"
+    elsif controller.action_name == "edit"
+       return "Update Article"
+    else
+       return "Submit"
+    end
+  end
 end
