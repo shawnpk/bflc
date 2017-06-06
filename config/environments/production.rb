5bc9c8f096ea962631f9d022cf6ae8ceb4b70a49
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-06-06 15:14:55 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :domain               => 'blfcwebsite.herokuapp.com'
+    :domain               => 'blfcwebsite.herokuapp.com',
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
