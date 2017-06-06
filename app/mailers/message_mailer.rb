ac3de4204c35120049c41239d8438451bcf65ae8
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-06-06 07:39:50 -0700

    Upate email settings

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,4 +1,4 @@
-class MessageMailer < ApplicationMailer::Base
+class MessageMailer < ApplicationMailer
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
