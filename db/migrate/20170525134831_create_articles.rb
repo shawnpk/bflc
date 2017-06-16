357c2213afae56331800fefcfa8e97485c2e3610
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-06-16 14:09:34 -0700

    Update app for DO deploy

--- db/migrate/20170525134831_create_articles.rb
+++ db/migrate/20170525134831_create_articles.rb
@@ -1,6 +1,6 @@
-class CreateArticles < ActiveRecord::Migration[5.1]
+class CreateBlogs < ActiveRecord::Migration[5.1]
   def change
-    create_table :articles do |t|
+    create_table :blogs do |t|
       t.string :title
       t.text :body
 
