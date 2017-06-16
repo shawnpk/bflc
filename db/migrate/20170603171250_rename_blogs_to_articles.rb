587fa70c9715a43ced58b2d80f2f10091898ca9c
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-06-16 14:25:29 -0700

    Update app for DO deploy

--- db/migrate/20170603171250_rename_blogs_to_articles.rb
+++
@@ -1,9 +0,0 @@
-class RenameBlogsToArticles < ActiveRecord::Migration[5.1]
-  def self.up
-    rename_table :blogs, :articles
-  end
-
-  def self.down
-    rename_table :articles, :blogs
-  end
-end
